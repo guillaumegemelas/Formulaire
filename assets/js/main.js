9731e65479b666acef94531a27f47c6523239e7a
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-01-28 08:25:20 +0100

    samedi matin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("document loaded");
+  try {
+    console.log("document loaded");
 
-  document.querySelector("form").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    //visu terminal
-    console.log("submit");
+    document.querySelector("form").addEventListener("submit", async (e) => {
+      e.preventDefault();
+      //visu terminal
+      console.log("submit");
 
-    const data = {
-      firstname: document.querySelector("#firstname").value,
-      lastname: document.querySelector("#lastname").value,
-      email: document.querySelector("#email").value,
-      subject: document.querySelector("#subject").value,
-      message: document.querySelector("#message").value,
-    };
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+      };
 
-    console.log(data);
+      console.log(data);
 
-    //requête vers le serveur eavec requete sur internet (pas en local) depuis url donnée pas Netlify:
-    const response = await axios.post("http://localhost:3000/form", data);
-    console.log(response);
-    //il faudra changer l'adresse si on veut remettre en local et faire test avec liveServeur du HTML
-    // const response = await axios.post("https://chipper-gelato-d55fff.netlify.app/form", data);
-    //
-    console.log(response);
-  });
+      //requête vers le serveur avec requete en local (port 3000)
+      const response = await axios.post("http://localhost:3000/form", data);
+
+      //il faudra changer l'adresse si on veut faire requete en ligne (pas en local) depuis url donnée pas Netlify:
+      // const response = await axios.post(
+      //   "https://chipper-gelato-d55fff.netlify.app/form",
+      //   data
+      // );
+      //
+      console.log(response);
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
