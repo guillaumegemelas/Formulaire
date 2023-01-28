d3f366f3b505cba719763bd28dfb1b745cfd2cab
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-01-28 01:25:14 +0100

    fin soirée marche local mais plus en ligne: erreur axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,13 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(data);
 
     //requête vers le serveur eavec requete sur internet (pas en local) depuis url donnée pas Netlify:
-    const response = await axios.post(
-      "https://chipper-gelato-d55fff.netlify.app/form",
-      data
-    );
+    const response = await axios.post("http://localhost:3000/form", data);
     console.log(response);
     //il faudra changer l'adresse si on veut remettre en local et faire test avec liveServeur du HTML
-    // const response = await axios.post("http://localhost:3000/form", data);
+    // const response = await axios.post("https://chipper-gelato-d55fff.netlify.app/form", data);
     //
+    console.log(response);
   });
 });
